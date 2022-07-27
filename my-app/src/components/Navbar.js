13bddf67c6007ed30002152f6d9118f73d801325
sreejithliterally <imsreejithofficial@gmail.com>
2022-07-27 19:13:25 +0530

    Added navbar component

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react"
+export default function Navbar(){
+    return ( 
+
+    <header className="Nav">
+   
+    <h2 className="Nav--title"> Stripts.</h2>
+
+<div className="Nav-s">
+    <ul className="Nav--item">About</ul>
+    <ul className="Nav--item">Services</ul>
+    <ul className="Nav--item">Contact</ul>
+    <ul className="Nav--item">Gallery</ul>
+    </div>
+    
+    </header>
+    )
+}
